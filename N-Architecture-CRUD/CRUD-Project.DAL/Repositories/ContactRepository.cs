0c862088a11f3b47bfe437e650aa811b2ea8ecfb
Soel Attala <soelattala7@gmail.com>
2023-08-27 22:40:45 -0300

    adding interfaces

---
+++ N-Architecture-CRUD/CRUD-Project.DAL/Repositories/ContactRepository.cs
@@ -0,0 +1,37 @@
+﻿using CRUD_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Project.DAL.Repositories
+{
+    public class ContactRepository : IGenericRepository<Contact>
+    {
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<Contact>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Insert(Contact model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Contact model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ N-Architecture-CRUD/CRUD-Project.DAL/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Project.DAL.Repositories
+{
+    public interface IGenericRepository<TEntityModel> where TEntityModel : class  //definimos que t entity models es una clase
+    {
+        Task<bool> Insert(TEntityModel model);
+        Task<bool> Update(TEntityModel model);
+        Task Delete(int id);
+        Task<bool> Get(int id);
+        Task<IQueryable<TEntityModel>> GetAll();
+        //cuando trabajamos con recurtsos de la base de datos es necesario usar IQueriable
+        ////when working with database resources it is necessary to use IQueriable
+    }
+}
